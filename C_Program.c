ad4a73f83e0c60d16d3f50a6e029a612d748daf0
teeguhwill@gmail.com <teeguhwill@gmail.com>
2021-05-09 22:16:32 +0000

    My First Cimmit

---
+++ C_Program.c
@@ -0,0 +1,9 @@
+#include<studio.h>
+
+iny main(){
+
+print("Hello!i am C-Program.Thank you!");
+
+return 0:
+
+}
---
+++ HelloJava.java
@@ -0,0 +1,9 @@
+class HelloJava {
+
+public static vopid main(string args[]){
+
+System.out.Println("Iam you Java Program. Thank You!");
+
+	}
+
+}
